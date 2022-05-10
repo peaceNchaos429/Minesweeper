c7eb38895b85595a25f62865b18fbc880d991980
Briana Buttone <BKButtone@student.fullsail.edu>
2022-05-10 17:13:05 -0400

    replaced 1 button with 100

--- Minesweeper/cMain.cpp
+++ Minesweeper/cMain.cpp
@@ -6,9 +6,20 @@ wxEND_EVENT_TABLE()
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Minesweeper",wxPoint(30,30), wxSize(800,600))
 {
-	m_btn1 = new wxButton(this, 10001, "Click Me", wxPoint(10, 10), wxSize(150, 50));
-	m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
-	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
+	btn = new wxButton * [nFieldWidth * nFieldHeight];
+	wxGridSizer* grid = new wxGridSizer(nFieldWidth, nFieldHeight, 0, 0);
+
+	for (int x = 0; x < nFieldWidth; x++)
+	{
+		for (int y = 0; y < nFieldHeight; y++)
+		{
+			btn[y * nFieldWidth + x] = new wxButton(this, 10000 + (y * nFieldWidth + x));
+			grid->Add(btn[y * nFieldWidth + x], 1, wxEXPAND | wxALL);
+		}
+	}
+
+	this->SetSizer(grid);
+	grid->Layout();
 }
 
 cMain::~cMain()
@@ -18,6 +29,6 @@ cMain::~cMain()
 
 void cMain::OnButtonClicked(wxCommandEvent& evt)
 {
-	m_list1->AppendString(m_txt1->GetValue());
+	
 	evt.Skip();
 }
