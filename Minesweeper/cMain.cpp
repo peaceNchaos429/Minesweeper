971cc56968f50f0c653b1b9ccbf73efc3ee8c527
Briana Buttone <BKButtone@student.fullsail.edu>
2022-05-10 16:55:01 -0400

    working button click (text is copied from textbox)

--- Minesweeper/cMain.cpp
+++ Minesweeper/cMain.cpp
@@ -1,10 +1,12 @@
 #include "cMain.h"
 
-
+wxBEGIN_EVENT_TABLE(cMain, wxFrame)
+	EVT_BUTTON(10001, OnButtonClicked)
+wxEND_EVENT_TABLE()
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Minesweeper",wxPoint(30,30), wxSize(800,600))
 {
-	m_btn1 = new wxButton(this, wxID_ANY, "Click Me", wxPoint(10, 10), wxSize(150, 50));
+	m_btn1 = new wxButton(this, 10001, "Click Me", wxPoint(10, 10), wxSize(150, 50));
 	m_txt1 = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 70), wxSize(300, 30));
 	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(10, 110), wxSize(300, 300));
 }
@@ -12,4 +14,10 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Minesweeper",wxPoint(30,30), wxSize
 cMain::~cMain()
 {
 
+}
+
+void cMain::OnButtonClicked(wxCommandEvent& evt)
+{
+	m_list1->AppendString(m_txt1->GetValue());
+	evt.Skip();
 }
--- Minesweeper/cMain.h
+++ Minesweeper/cMain.h
@@ -8,8 +8,12 @@ public:
 	~cMain();
 
 public:
-	wxButton* m_btn1 = nullptr;
-	wxTextCtrl* m_txt1 = nullptr;
-	wxListBox* m_list1 = nullptr;
+	wxButton *m_btn1 = nullptr;
+	wxTextCtrl *m_txt1 = nullptr;
+	wxListBox *m_list1 = nullptr;
+
+	void OnButtonClicked(wxCommandEvent &evt);
+
+	wxDECLARE_EVENT_TABLE();
 };
 
