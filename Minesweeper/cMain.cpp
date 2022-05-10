af9ff78427f3c8e866d3abc070a4e760bb2b4679
Briana Buttone <BKButtone@student.fullsail.edu>
2022-05-10 17:23:24 -0400

    working buttons

--- Minesweeper/cMain.cpp
+++ Minesweeper/cMain.cpp
@@ -1,7 +1,7 @@
 #include "cMain.h"
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
-	EVT_BUTTON(10001, OnButtonClicked)
+	//EVT_BUTTON(10001, OnButtonClicked)
 wxEND_EVENT_TABLE()
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Minesweeper",wxPoint(30,30), wxSize(800,600))
@@ -15,6 +15,8 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Minesweeper",wxPoint(30,30), wxSize
 		{
 			btn[y * nFieldWidth + x] = new wxButton(this, 10000 + (y * nFieldWidth + x));
 			grid->Add(btn[y * nFieldWidth + x], 1, wxEXPAND | wxALL);
+
+			btn[y * nFieldWidth + x]->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &cMain::OnButtonClicked, this);
 		}
 	}
 
@@ -24,11 +26,13 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Minesweeper",wxPoint(30,30), wxSize
 
 cMain::~cMain()
 {
-
+	delete[]btn;
 }
 
 void cMain::OnButtonClicked(wxCommandEvent& evt)
 {
-	
+	//get coordinate of button in field array
+	int x = (evt.GetId() - 10000) % nFieldWidth;
+	int y = (evt.GetId() - 10000) / nFieldWidth;
 	evt.Skip();
 }
